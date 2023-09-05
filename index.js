bfd412718313204cb8ec4ba8e9e602fdfb05d648
Yasin <yasmu213@gmail.com>
2023-09-04 22:20:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Perform three calculations using different arithmetic operators.
+let code1 = 2 * 5; // 10 Multiplication
+let code2 = 20 + 20; // 40 Addition
+let code3 = 40 - 1; // 39 Subtraction
+
+// Create a string.
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+// Combine the into a string
+let codes = code1 + " - " + code2 + " - " + code3;
+
+// Display result in an alert window
+alert(message + codes);
